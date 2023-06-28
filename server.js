926c12b155f72da227a0a5969c82b4bebd43767d
Benreefgit config --global core.editor code <ben@outrun.io>
2023-06-29 08:52:23 +0930

    render deployment ready

--- server.js
+++ server.js
@@ -32,14 +32,14 @@ app.use('/api/games', gamesController)
 app.use('submit_data', dataController)
 
 
-// if (process.env.NODE_ENV === 'production') {
-//   const path = require('path')
-//   app.use(express.static(path.join(__dirname, 'build')));
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path')
+  app.use(express.static(path.join(__dirname, 'build')));
 
-//   app.get('/*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-//   });
-// }
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+}
 
 // let netBallAPI = []
 
